952a0be5fd943a8a8f4dfa59c1ac941f8846f812
saff-it <saff_it@ymail.com>
2022-09-09 16:26:38 +0200

    migration created

---
+++ database/migrations/2022_09_09_140529_create_train_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('train', function (Blueprint $table) {
+            $table->id();
+            $table->string('company_name');
+            $table->string('departure_station');
+            $table->string('arrival_station');
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->string('train_code');
+            $table->tinyInt('coach_number');
+            $table->string('is_on_time');
+            $table->string('is_cancelled');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('train');
+    }
+}
